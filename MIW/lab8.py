200c167f7d42c17f9786dc61c8dcadec937fe5c2
MackiewiczT26 <mackiewiczt26@gmail.com>
2022-06-08 18:31:07 +0200

    lab8

--- MIW/lab8.py
+++ MIW/lab8.py
@@ -1,24 +1,19 @@
-from lab7 import projection, matrix_length, split_into_q_r 
-import math
 import numpy as np
-
-def next_matrix(matrix,q,r):
-    split_into_q_r(matrix, q, r)
-    new_matrix = np.dot(np.array(q).T, matrix)
-    return np.dot(new_matrix, q)
+from lab7 import split_into_q_r
 
 def eigenvalues(matrix):
-    new_matrix = np.array(matrix)
-    i = 0
-    while (np.diag(new_matrix)-np.dot(new_matrix, np.ones((matrix.shape))).T).all()>0.001:
-        q = []
-        new_matrix = next_matrix(new_matrix, q, r)
-        i = i + 1
-        print("Eigen val nr "+str(i)+": ", new_matrix, sep="\n")
-    return np.diag(new_matrix)
+    vals = np.array(matrix)
+    print(matrix)
+    q = []
+    r = []
+    split_into_q_r(matrix, q, r)
+    vals = np.dot(r, q)
+    vals = (list(x) for x in vals)
+    return vals
 
-sample = np.array([[1.,2.,3.,4.,5.],[2.,2.,3.,4.,5.],[3.,3.,3.,4.,5.],[4.,4.,4.,4.,5.],[5.,5.,5.,5.,5.]])
-q=[]
-r=[]
 
-print(eigenvalues(sample))
+sample = np.array([[1,1,0], [1,0,1], [0,1,1]])
+
+eigen = eigenvalues(sample)
+
+print([x for x in eigen])
